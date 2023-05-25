f038450062549d3a1540d1558f3a593acd464881
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-05-25 16:33:56 +0200

    [FEAT]stampa messaggio in dom, sostituito prompt

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -1,37 +1,40 @@
-
+// recupero la parola inserita dall'utente
 const parola = document.getElementById('parola').value;
 
 console.log(parola);
-
-let lettere = parola.split('')
-console.log(lettere)
-
-let lettereReverse = lettere.reverse('')
-console.log(lettereReverse)
-
-let lettereJoin = lettereReverse.join('')
-console.log(lettereJoin)
-
-function palindroma (control) {
-
-    let lettere = parola.split('')
-    console.log(lettere)
-
-    let lettereReverse = lettere.reverse('')
-    console.log(lettereReverse)
+// collego il confronto al click del pulsante
+let button = document.getElementById('search')
+button.addEventListener('click', function(){
+    // creo la funzione per confrontare le due parole
+    function palindroma (controllo) {
+        let lettere = parola.split('');
+        console.log(lettere);
     
-    let lettereJoin = lettereReverse.join('')
-    console.log(lettereJoin)
-
-    if (lettereJoin === parola) {
-        return
-        control === true
+        let lettereReverse = lettere.reverse('');
+        console.log(lettereReverse);
+    
+        let lettereJoin = lettereReverse.join('');
+        console.log(lettereJoin);
+    
+        if (lettereJoin === controllo) {
+            return 'palindromo'
+            
+        } else{
+            return 'non palindromo'
+             
+        }
+    };
+    
+    // stampo in pagina il risultato
+    let confronto = palindroma(parola);
+    if (palindroma(parola) === 'palindromo'){
+        console.log ('La parola è un palindromo')
+        document.getElementById('messaggio').innerText= 'La parola è un palindromo'
+    } else {
+        console.log ('La parola è un palindromo')
+        document.getElementById('messaggio').innerText= 'La parola non è un palindromo'
     }
-
-    return
-    control === false
-
-};
+})
 
 
-let confronto = palindroma()
+
