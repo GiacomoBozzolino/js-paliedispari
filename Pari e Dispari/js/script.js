4a0e1b1448902d4c09d6017300508e1f5b187180
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-05-25 17:31:49 +0200

    [FEAT]lavoro confronto scelta utente

--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -1,11 +1,48 @@
 
-let button = document.getElementById('play')
+let button = document.getElementById('play');
 button.addEventListener('click', function(){ 
-    const scelta = document.getElementById('scelta').value
-    const number = document.getElementById('number').value
-    console.log(scelta)
-    console.log(number)
+    const scelta = document.getElementById('scelta').value;
+    const number = document.getElementById('number').value;
+    console.log(scelta);
+    console.log(number);
+
+    function randomNumber (min, max) {
+       return Math.floor(Math.random() * ( max-min +1) + min);
+       
+       
+    }
+
+    let num = randomNumber (1, 5);
+    console.log(num)
+
+   function game (somma) {
+    if  ((somma + number) % 2 === 0){
+         return 'pari'
+         
+
+    } 
+         return 'dispari'
+   }
+
+ let risultato = game (num)
+
+ console.log(risultato)
+   if (risultato === number){
+       console.log ('hai vinto')
+
+   } else {
+    console.log ('hai perso')
+   }
 
 
 
-})
+
+}) 
+
+
+
+
+
+
+
+    
