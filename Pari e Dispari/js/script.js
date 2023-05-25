9f5b745881297ff003e70290718e4d6d2f158a5b
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-05-25 17:42:01 +0200

    [FEAT] aggiunto parseInt a input utente numeri

--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -1,41 +1,38 @@
-
 let button = document.getElementById('play');
 button.addEventListener('click', function(){ 
     const scelta = document.getElementById('scelta').value;
-    const number = document.getElementById('number').value;
+    const number = parseInt( document.getElementById('number').value);
     console.log(scelta);
     console.log(number);
 
     function randomNumber (min, max) {
-       return Math.floor(Math.random() * ( max-min +1) + min);
-       
-       
+       return Math.floor(Math.random() * ( max-min +1) + min);  
     }
 
     let num = randomNumber (1, 5);
     console.log(num)
+    console.log(num+number)
 
    function game (somma) {
     if  ((somma + number) % 2 === 0){
          return 'pari'
          
+    } else {
+        return 'dispari'
 
-    } 
-         return 'dispari'
+    }
    }
 
- let risultato = game (num)
+    let risultato = game (num)
 
- console.log(risultato)
-   if (risultato === number){
+    console.log(risultato)
+
+   if (risultato === scelta){
        console.log ('hai vinto')
 
    } else {
     console.log ('hai perso')
    }
-
-
-
 
 }) 
 
